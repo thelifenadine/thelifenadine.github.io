d34eba8380b7712bd2ecb37249ac92f348f2ee94
Nadine Kinnen <nadine@cotap.com>
2016-01-18 17:41:49 -0800

    Cleanup, move the lightbox title to update only when image is loaded

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 (function() {
   'use strict';
 
-  var application = {};
-
-  application.feedback = document.getElementById('feedback');
+  var application = {
+    rootUrl: 'https://api.flickr.com/services/rest/?',
+    apiKey: '2a77b915bc4f3fd5dc2ae316c3d4c28f',
+    feedbackElement: document.getElementById('feedback')
+  };
 
   application.photoService = function() {
-    var apiKey = '2a77b915bc4f3fd5dc2ae316c3d4c28f';
-    var galleryId = '72157663337771571';
-
     return {
       loadPhotos: function(callback) {
-        var endpoint = 'https://api.flickr.com/services/rest/?'
+        var galleryId = '72157663337771571';
+        var endpoint = application.rootUrl
           + 'method=flickr.galleries.getPhotos'
-          + '&api_key=' + apiKey
+          + '&api_key=' + application.apiKey
           + '&gallery_id='+ galleryId
           + '&format=json&nojsoncallback=1';
 
@@ -26,7 +26,7 @@
               var originalPhotoList = data.photos.photo;
               callback(data.photos.photo);
             } else {
-              application.feedback.textContent = "Error loading photos";
+              application.feedbackElement.textContent = "Error loading photos";
             }
           }
         };
@@ -52,7 +52,7 @@
     };
 
     var createPhotoGrid = function(photos) {
-      application.feedback.textContent = "Loading...";
+      application.feedbackElement.textContent = "Loading...";
 
       var photoWrapper = document.getElementById('photo-wrapper');
       var photoList = document.createElement('div')
@@ -72,9 +72,8 @@
         photoList.appendChild(img);
       });
 
-      //hit the dom once, not on every photo
       photoWrapper.appendChild(photoList);
-      application.feedback.textContent = "";
+      application.feedbackElement.textContent = "";
     };
 
     var changeCurrentPhoto = function(photo) {
@@ -85,14 +84,16 @@
 
       var lightboxImg = document.getElementById('lightbox-img');
 
+      lightboxImg.addEventListener("load", function() {
+        document.getElementById('lightbox-title').textContent = photo.title;
+      }, false);
+
       var src = document.createAttribute('src');
       src.value = 'https://farm'
         + photo.farm +'.staticflickr.com/' + photo.server +'/'
-        + photo.id +'_' + photo.secret +'_c.jpg'
+        + photo.id +'_' + photo.secret +'_b.jpg'
 
-      //update the lightbox image and title
       lightboxImg.setAttributeNode(src);
-      document.getElementById('lightbox-title').textContent = photo.title;
     };
 
     var onClickImage = function(photo) {
@@ -119,8 +120,6 @@
     };
 
     var addEventListeners = function() {
-      var self = this;
-
       document.getElementById('lightbox-next')
         .addEventListener('click', function(){ onClickLightBoxNext() }, true);
 
@@ -133,11 +132,8 @@
 
     return {
       initialize: function() {
-        lightboxDiv = document.getElementById('lightbox-wrapper');
-        currentPhoto = null;
-
-        addEventListeners();
         application.photoService().loadPhotos(createPhotoGrid);
+        addEventListeners();
       }
     }
   };
